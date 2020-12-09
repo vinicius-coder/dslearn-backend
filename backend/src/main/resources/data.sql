607c788b9c7f429b84e94c346d6d37dbe803e594
Avonts <vinialves022@gmail.com>
2020-12-08 22:08:06 -0300

    Add Course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -3,12 +3,14 @@ INSERT INTO tb_user (name, email, password) VALUES ('Maria Green', 'maria@gmail.
 INSERT INTO tb_user (name, email, password) VALUES ('Bob Brown', 'bob@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_INSTRUCTOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
-
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML','https://cdn.pixabay.com/photo/2018/03/22/10/55/training-course-3250007_1280.jpg','https://cdn.pixabay.com/photo/2018/03/22/10/55/training-course-3250007_1280.jpg');

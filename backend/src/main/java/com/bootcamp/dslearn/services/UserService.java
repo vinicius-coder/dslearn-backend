0ed87d0a3fd112f050bb0f70a72e2ffb913b1c0d
Avonts <vinialves022@gmail.com>
2020-12-10 21:46:23 -0300

    Add validation and exception handling

--- backend/src/main/java/com/bootcamp/dslearn/services/UserService.java
+++ backend/src/main/java/com/bootcamp/dslearn/services/UserService.java
@@ -1,5 +1,7 @@
 package com.bootcamp.dslearn.services;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +9,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.bootcamp.dslearn.dto.UserDTO;
 import com.bootcamp.dslearn.entities.User;
 import com.bootcamp.dslearn.repositories.UserRepository;
+import com.bootcamp.dslearn.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -18,6 +23,13 @@ public class UserService implements UserDetailsService {
 	
 	@Autowired
 	private UserRepository repository;
+	
+	@Transactional(readOnly = true)
+	public UserDTO findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new UserDTO(entity);
+	}
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
